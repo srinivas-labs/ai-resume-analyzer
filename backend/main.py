11f8043adf6197d345ac95e2b7871dadf9aa9b75
AI Resume Analyzer User <user@example.com>
2026-05-10 23:54:48 -0400

    Initial commit: AI Resume Analyzer project setup

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
+
+app = FastAPI()
+
+UPLOAD_FOLDER = "uploads"
+
+# Create uploads folder if not exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+@app.get("/")
+def home():
+    return {"message": "AI Resume Analyzer Backend Running Successfully"}
+
+
+@app.post("/upload-resume")
+async def upload_resume(file: UploadFile = File(...)):
+    
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "Resume uploaded successfully",
+        "file_name": file.filename
+    }
